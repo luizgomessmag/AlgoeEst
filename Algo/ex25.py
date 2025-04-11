1cf97c47be282e952b972f230cfa9fb87e9899d5
Luiz Henrique Gomes <lhgk22235@gmail.com>
2025-04-10 20:50:14 -0400

    Add files via upload

---
+++ Algo/ex25.py
@@ -0,0 +1,19 @@
+sorteio=int(input("escolha um numero de 1 a 20 "))
+palpites=[]
+palpite=0
+
+palpite=int(input("insira seu palpite "))
+palpites.append(palpite)
+
+while palpite!=sorteio:
+
+    palpite=int(input("insira seu palpite "))
+    palpites.append(palpite)
+        
+    print("você errou")
+    
+
+else:
+    print(f"você acertou, o valor era: {sorteio}")
+    print(f"seus palpites foram:{palpites}")
+
